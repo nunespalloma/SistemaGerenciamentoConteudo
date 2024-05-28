ebf926fe178ba783c3ecb18bec9039fad291c354
TiamatCod3 <ti_matos@id.uff.br>
2024-05-28 18:02:13 -0300

    feat: atualização do swagger de usuários e espaços

--- src/main/java/br/uff/ic/controller/EspacoController.java
+++ src/main/java/br/uff/ic/controller/EspacoController.java
@@ -2,6 +2,8 @@ package br.uff.ic.controller;
 
 import br.uff.ic.model.Espaco;
 import br.uff.ic.repository.EspacoRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +16,14 @@ public class EspacoController {
     @Autowired
     private EspacoRepository espacoRepository;
 
+    @Operation(summary = "Cadastrar novo Espaço",
+            description = "Cadastra um novo espaço no banco de dados do sistema. Requer um organizador autorizado no "
+                    + "corpo da requisição.",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Espaço criado com sucesso"),
+                    @ApiResponse(responseCode = "400", description = "Dados inválidos"),
+                    @ApiResponse(responseCode = "403", description = "Usuário não autorizado")
+            })
     @PostMapping("/criarEspaco")
     public Espaco criarEspaco (@RequestBody Espaco espaco){
         return espacoRepository.save(espaco);
